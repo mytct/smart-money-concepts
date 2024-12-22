a8655982d5a708ec8d2529e961d8594242668520
mytct <mtoanmy91>
2024-12-22 21:52:54 +0700

    feat: update colors

--- analyze.py
+++ analyze.py
@@ -1,12 +1,21 @@
 import pandas as pd
 import numpy as np
 from smartmoneyconcepts import smc
-import plotly.graph_objects as go
+from termcolor import colored
+# import plotly.graph_objects as go
+
+# pip install pandas
+# pip install requests
+# pip install termcolor
+
+def print_c(text, color="red"):
+     print(colored(text, color))
 
 # Step 1: Create a Sample OHLC DataFrame
 def generate_sample_data():
-    print("Step 1: File loaded successfully.")
-    df = pd.read_csv("data/btc.csv", parse_dates=["datetime"])
+    symbol = "FTM"
+    print_c('Step 1: File loaded successfully.')
+    df = pd.read_csv(f"data/{symbol}.csv", parse_dates=["datetime"])
     # Convert column names to lowercase
     df.columns = df.columns.str.lower()
     # df.rename(columns={'price': 'close'}, inplace=True)
@@ -30,64 +39,64 @@ def generate_sample_data():
 def main():
     # Generate and print the sample OHLCV data
     ohlcv_data = generate_sample_data()
-    print("Sample OHLCV DataFrame:")
-    print(ohlcv_data.head())
+    print_c("Sample OHLCV DataFrame:", 'green')
+    print_c(ohlcv_data.head(), 'blue')
     print("============================")
-    print("Step 2: Starting FVG calculation...")
+    print_c("Step 2: Starting FVG calculation...")
     # FVG Calculation
     try:
         fvgResult = smc.fvg(ohlcv_data, join_consecutive=False)
         # print("\n FVG Result:", fvgResult)
-        print("\n FVG Columns:", fvgResult.columns)
+        # print("\nFVG Columns:", fvgResult.columns)
         fvg_filtered_result = fvgResult.dropna(subset=['FVG'])
 
         if not fvg_filtered_result.empty:
             print("\nFiltered FVG Result (non-NaN values):")
-            print(fvg_filtered_result)
+            print_c(fvg_filtered_result, 'blue')
         else:
             print("\nNo valid Fair Value Gap (FVG) records found.")
     
     except Exception as e:
-        print(f"Error occurred: {e}")
+        print_c(f"Error occurred: {e}")
 
     print("============================")
-    print("Step 3: SHL calculation...")
+    print_c("Step 3: SHL calculation...", 'green')
     # SHL Calculation
     try:
         shlResult = smc.swing_highs_lows(ohlcv_data, swing_length = 50)
         # print("\n SHL Result:", shlResult)
-        print("\n SHL Columns:", shlResult.columns)
+        # print("\nSHL Columns:", shlResult.columns)
         shl_filtered_result = shlResult.dropna(subset=['HighLow'])
 
         if not shl_filtered_result.empty:
             print("\nFiltered SHL Result (non-NaN values):")
-            print(shl_filtered_result)
+            print_c(shl_filtered_result, 'blue')
         else:
             print("\nNo valid Swing Highs and Lows (SHL) records found.")
     
     except Exception as e:
-        print(f"Error occurred: {e}")
+        print_c(f"Error occurred: {e}")
 
     print("============================")
-    print("Step 4: Break of Structure (BOS) & Change of Character (CHoCH) calculation...")
+    print_c("Step 4: Break of Structure (BOS) & Change of Character (CHoCH) calculation...", 'green')
     # Break of Structure (BOS) & Change of Character (CHoCH) Calculation
     try:
         bosChochResult = smc.bos_choch(ohlcv_data, shlResult, close_break = True)
         # print("\n BOS_CHOCH Result:", bosChochResult)
-        print("\n BOS_CHOCH Columns:", bosChochResult.columns)
+        # print("\nBOS_CHOCH Columns:", bosChochResult.columns)
         bos_choch_filtered_result = bosChochResult.dropna(subset=['BOS'])
 
         if not bos_choch_filtered_result.empty:
             print("\nFiltered BOS_CHOCH Result (non-NaN values):")
-            print(bos_choch_filtered_result)
+            print_c(bos_choch_filtered_result, 'blue')
         else:
-            print("\nNo valid Break of Structure (BOS) & Change of Character (CHoCH) Calculation (BOS_CHOCH) records found.")
+            print_c("\nNo valid Break of Structure (BOS) & Change of Character (CHoCH) Calculation (BOS_CHOCH) records found.")
     
     except Exception as e:
         print(f"Error occurred: {e}")
 
     print("============================")
-    print("Step 5: Order Blocks calculation...")
+    print_c("Step 5: Order Blocks calculation...", 'green')
     # Order Blocks (OB)
     # try:
     #     obResult = smc.ob(ohlcv_data, shlResult, close_mitigation = False)
@@ -105,25 +114,25 @@ def main():
     #     print(f"Error occurred: {e}")
 
     print("============================")
-    print("Step 6: Liquidity calculation...")
+    print_c("Step 6: Liquidity calculation...", 'green')
     # Liquidity
     try:
         lqResult = smc.liquidity(ohlcv_data, shlResult, range_percent = 0.01)
         # print("\n Liquidity Result:", lqResult)
-        print("\n Liquidity Columns:", lqResult.columns)
+        # print("\nLiquidity Columns:", lqResult.columns)
         lq_filtered_result = lqResult.dropna(subset=['Liquidity'])
 
         if not lq_filtered_result.empty:
             print("\nFiltered Liquidity Result (non-NaN values):")
-            print(lq_filtered_result)
+            print_c(lq_filtered_result, 'blue')
         else:
-            print("\nNo valid Liquidity records found.")
+            print_c("\nNo valid Liquidity records found.")
     
     except Exception as e:
         print(f"Error occurred: {e}")
 
     print("============================")
-    print("Step 7: Previous High And Low calculation...")
+    print_c("Step 7: Previous High And Low calculation...", 'green')
     # Previous High And Low
     # try:
     #     pHLResult = smc.previous_high_low(ohlcv_data, time_frame = "1D")
@@ -141,7 +150,7 @@ def main():
     #     print(f"Error occurred: {e}")
 
     print("============================")
-    print("Step 8: Sessions calculation...")
+    print_c("Step 8: Sessions calculation...", 'green')
     # Sessions
     # try:
     #     session = "London"
@@ -162,37 +171,37 @@ def main():
     #     print(f"Error occurred: {e}")
 
     print("============================")
-    print("Step 9: Retracments calculation...")
+    print_c("Step 9: Retracments calculation...", 'green')
      # Retracements
     try:
         reResult = smc.retracements(ohlcv_data, shlResult)
         # print("\n Retracements:", reResult)
-        print("\n Retracements:", reResult.columns)
+        # print("\nRetracements:", reResult.columns)
         retracement_filtered_result = reResult.dropna(subset=['Direction'])
 
         if not retracement_filtered_result.empty:
             print("\nFiltered Retracements (non-NaN values):")
-            print(retracement_filtered_result)
+            print_c(retracement_filtered_result, 'blue')
         else:
-            print("\nNo valid Retracements records found.")
+            print_c("\nNo valid Retracements records found.")
     
     except Exception as e:
         print(f"Error occurred: {e}")
 
     print("============================")
-    fig = go.Figure(data=[go.Candlestick(x=ohlcv_data['datetime'],
-                                     open=ohlcv_data['open'],
-                                     high=ohlcv_data['high'],
-                                     low=ohlcv_data['low'],
-                                     close=ohlcv_data['close'])])
+    # fig = go.Figure(data=[go.Candlestick(x=ohlcv_data['datetime'],
+    #                                  open=ohlcv_data['open'],
+    #                                  high=ohlcv_data['high'],
+    #                                  low=ohlcv_data['low'],
+    #                                  close=ohlcv_data['close'])])
 
-    # Set the title and labels
-    fig.update_layout(title="Candlestick Chart Example",
-                  xaxis_title="Date",
-                  yaxis_title="Price")
+    # # Set the title and labels
+    # fig.update_layout(title="Candlestick Chart Example",
+    #               xaxis_title="Date",
+    #               yaxis_title="Price")
 
-    # Show the chart
-    fig.show()
+    # # Show the chart
+    # fig.show()
     
     # Save the DataFrame for use with SMC or further processing
     # ohlcv_data.to_csv("sample_ohlcv.csv", index=True)  # Save with date index
--- download.py
+++ download.py
@@ -2,22 +2,32 @@ import requests
 
 # Make the GET request
 # Define the symbol as a variable
-symbol = "BTC/USD"
+symbol = "FTM/USD"
+f_symbol = "FTM"
 start_date = "2024-12-01 00:00:00"
 end_date = "2024-12-22 20:30:00"
-interval = "2024-12-22 20:30:00"
+interval = "15min"
 
 # https://twelvedata.com/account/api-playground
 
 # Create the URL dynamically
 url = f"https://api.twelvedata.com/time_series?apikey=4985500cb16646ee94f6ab2b4a90f2a1&interval={interval}&symbol={symbol}&start_date={start_date}&end_date={end_date}&format=CSV"
 
+# Make the GET request
+response = requests.get(url)
+
 # Check if the request was successful
 if response.status_code == 200:
+    # Replace semicolons with commas if necessary
     csv_data = response.text.replace(";", ",")
+    
+    # Ensure the 'data' directory exists (create it if not)
+    import os
+    os.makedirs("data", exist_ok=True)
+
     # Open a file in write mode and save the response as CSV
-    with open("data/btc.csv", "w") as file:
-        file.write(response.text)
-    print("Data has been written to btc_data.csv")
+    with open(f"data/{f_symbol}.csv", "w") as file:
+        file.write(csv_data)
+    print(f"Data has been written to data/{f_symbol}.csv")
 else:
     print(f"Error: {response.status_code}")
